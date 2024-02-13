309a16e65f1fe2f1c1f5dae7f4f6763813e4fb20
Styaaa <s160421048@student.ubaya.ac.id>
2024-02-14 01:12:01 +0800

    Final

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+// import swal from 'sweetalert';
+
 onload = () => {
   
   document.body.classList.remove("container");
-  alert("Selamat Valentine Cey\n- Satya");
+  // alert("Selamat Valentine Cey\n- Satya", "Helooo");
+  swal({
+    title: "Guess What?",
+    text: "Happy Valentine Cey!!",
+    button: "Aww yiss!",
+  });
 };
 
 // $(".button-51").click(function() {
